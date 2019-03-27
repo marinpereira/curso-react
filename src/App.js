ef13dad88ee1dedcd8f4c827fdc85f9848279c52
Luiz Paulo Marin Pereira <marinpereira@uol.com.br>
2019-03-27 15:40:49 -0300

    Criação do Estilo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Simples texto='Flexível !!'/>
+        <Simples texto='Flexível Amigão'/>
       </View>
     )
   }
--- src/componentes/Simples.js
+++ src/componentes/Simples.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import {Text} from 'react-native'
+import Padrao from '../estilo/Padrao'
 
-export default function(props){
-  return <Text>{props.texto}</Text>
-}
+// inicial
+// export default function(props){
+//   return <Text>{props.texto}</Text>
+// }
+
+// 1 mudança Arrow function =>
+// export default (props) =>{
+//     return <Text>Arrow: {props.texto}</Text>
+// }
+
+// 2 Mudança Arrow function e um parametro
+// export default props =>{
+//     return <Text>Arrow: {props.texto}</Text>
+// }
+
+// 3 Mudança Arrow function retirando {} e return
+export default props => 
+    <Text style={[Padrao.ex]}>Arrow: {props.texto}</Text>
